400734bf73ad37930f54d916e52ac90088bc0794
benn <sjembn@gmail.com>
2018-04-13 08:32:07 +0800

    add scaffold

---
+++ bigbrother/main.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+bigbrother.main
+~~~~~~~~~~~~
+
+Providers the entrance
+"""
+import os
+import argparse
+
+from flask import Flask
+
+templateDir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+
+app = Flask(__name__, template_folder=templateDir)
+app.debug = bool(os.environ.get('DEBUG'))
+
+@app.route('/')
+def home_page():
+    return 'this is the home page'
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", type=int, default=5000)
+    parser.add_argument("--host", default="127.0.0.1")
+    args = parser.parse_args()
+    app.run(port=args.port, host=args.host)
---
+++ tests/test_bigbrother.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+def test_test():
+    assert(1==1)
